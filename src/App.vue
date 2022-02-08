a0700ab14014094dd7af9c4274eaa13724366d73
k.taniuchi <barbeekonta1995@gmail.com>
2022-02-08 22:21:37 +0900

    App.vueにvueのロゴを挿入、Home.vueをLogin.vueに変更

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
-    <router-view/>
+    <div class="container mx-auto mt-5">
+      <img alt="Vue logo" src="@/assets/logo.png" class="mx-auto" />
+    </div>
+    <router-view />
   </div>
 </template>
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,6 +1,5 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" class="flex justify-center">
     <p class="text-center">テスト</p>
   </div>
 </template>
