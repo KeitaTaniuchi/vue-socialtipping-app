261d617c96ea334d44459e57738846e6876df235
k.taniuchi <barbeekonta1995@gmail.com>
2022-02-17 22:32:20 +0900

    registerUserメソッドをactionsに移動

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,35 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
+  state: {},
+  mutations: {},
   actions: {
+    registerUser(context, { email, password }) {
+      createUserWithEmailAndPassword(getAuth(), email, password)
+        .then((userCredential) => {
+          this.$router.push("/");
+          const user = userCredential.user;
+          user.updateProfile({
+            displayName: this.userName,
+          });
+        })
+        .catch((error) => {
+          console.log(error.code);
+          /*  if (error.code === "auth/email-already-in-use") {
+            this.emailError = "このメールアドレスは既に使用されています";
+          }
+          if (error.code === "auth/invalid-email") {
+            this.emailError = "無効なメールアドレスが入力されています";
+          }
+          if (error.code === "auth/weak-password") {
+            this.passwordError = "このパスワードは貧弱すぎます";
+          } */
+        });
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
--- src/views/UserRegistration.vue
+++ src/views/UserRegistration.vue
@@ -46,7 +46,6 @@
 import AccountRelatedButton from "../components/AccountRelatedButton.vue";
 import FormInput from "../components/FormInput.vue";
 import TogglePasswordDisplayButton from "../components/TogglePasswordDisplayButton.vue";
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 export default {
   name: "UserRegistration",
   components: { FormInput, AccountRelatedButton, TogglePasswordDisplayButton },
@@ -64,26 +63,10 @@ export default {
     registerUser() {
       this.emailError = "";
       this.passwordError = "";
-      createUserWithEmailAndPassword(getAuth(), this.email, this.password)
-        .then((userCredential) => {
-          this.$router.push("/");
-          const user = userCredential.user;
-          user.updateProfile({
-            displayName: this.userName,
-          });
-        })
-        .catch((error) => {
-          console.log(error.code);
-          if (error.code === "auth/email-already-in-use") {
-            this.emailError = "このメールアドレスは既に使用されています";
-          }
-          if (error.code === "auth/invalid-email") {
-            this.emailError = "無効なメールアドレスが入力されています";
-          }
-          if (error.code === "auth/weak-password") {
-            this.passwordError = "このパスワードは貧弱すぎます";
-          }
-        });
+      this.$store.dispatch("registerUser", {
+        email: this.email,
+        password: this.password,
+      });
     },
   },
 };
