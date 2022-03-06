90c8fb9a5a45a020ed3fed78533240818de77638
k.taniuchi <barbeekonta1995@gmail.com>
2022-03-06 19:10:58 +0900

    ダッシュボードにログアウトボタンを追加

--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -2,7 +2,15 @@
   <div class="container mx-auto">
     <div class="flex w-6/12 mb-20 justify-between mx-auto">
       <p>ようこそ{{ currentUserName }}さん</p>
-      <p>残高:400</p>
+      <div>
+        <p>残高：400</p>
+        <AccountRelatedButton
+          @parentEvent="logout"
+          label="ログアウト"
+          type="button"
+          class="mt-4 block py-1"
+        />
+      </div>
     </div>
 
     <p class="mb-20 text-center text-4xl">ユーザー一覧</p>
@@ -11,9 +19,10 @@
 </template>
 
 <script>
+import AccountRelatedButton from "../components/AccountRelatedButton.vue";
 export default {
   name: "DashBoard",
-  components: {},
+  components: { AccountRelatedButton },
   data() {
     return {};
   },
