3fec7fd2c7c6ccd48648c54e16e03b8bd42d0cef
k.taniuchi <barbeekonta1995@gmail.com>
2022-03-15 20:43:06 +0900

    未ログイン時にダッシュボード画面に遷移した際に表示させるモーダルをコンポーネント化

---
+++ src/components/Modal/NotLoginWarning.vue
@@ -0,0 +1,35 @@
+<template>
+  <modal
+    name="notLoginWarning"
+    @before-open="threeSecondsAfterGoLoginPage"
+    :resizable="false"
+    :draggable="false"
+    :width="auto"
+    :height="auto"
+    :clickToClose="false"
+    class="text-center"
+  >
+    <div class="p-10">
+      <h1 class="text-red-500 text-4xl">警告</h1>
+      <div class="pt-10 text-red-500 text-2xl">
+        <p>あなたはログインしていません</p>
+        <p>先にログインしてください</p>
+      </div>
+      <p class="pt-10">3秒後に自動的にログイン画面に遷移します</p>
+    </div>
+  </modal>
+</template>
+
+<script>
+export default {
+  name: "NotLoginWarning",
+  methods: {
+    threeSecondsAfterGoLoginPage() {
+      setTimeout(this.goLoginPage, 3000);
+    },
+    goLoginPage() {
+      this.$router.push({ path: "/" });
+    },
+  },
+};
+</script>
--- src/views/DashBoard.vue
+++ src/views/DashBoard.vue
@@ -65,21 +65,7 @@
       <LoadingAnimation />
     </div>
 
-    <modal
-      name="notLoginWarning"
-      @before-open="threeSecondsAfterGoLoginPage"
-      :resizable="false"
-      :draggable="false"
-      :clickToClose="false"
-      class="text-center"
-    >
-      <h1 class="mt-8 text-red-500 text-4xl">警告</h1>
-      <div class="mt-8 text-red-500 text-2xl">
-        <p>あなたはログインしていません</p>
-        <p>先にログインしてください</p>
-      </div>
-      <p class="mt-8">3秒後に自動的にログイン画面に遷移します</p>
-    </modal>
+    <NotLoginWarningModal />
   </div>
 </template>
 
@@ -87,10 +73,16 @@
 import AccountRelatedButton from "../components/AccountRelatedButton.vue";
 import LoadingAnimation from "../components/LoadingAnimation.vue";
 import WalletRelatedButton from "../components/WalletRelatedButton.vue";
+import NotLoginWarningModal from "../components/Modal/NotLoginWarning.vue";
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 export default {
   name: "DashBoard",
-  components: { AccountRelatedButton, LoadingAnimation, WalletRelatedButton },
+  components: {
+    AccountRelatedButton,
+    LoadingAnimation,
+    WalletRelatedButton,
+    NotLoginWarningModal,
+  },
   data() {
     return {};
   },
@@ -121,12 +113,6 @@ export default {
   methods: {
     logout() {
       this.$store.dispatch("Logout/logout");
-    },
-    threeSecondsAfterGoLoginPage() {
-      setTimeout(this.goLoginPage, 3000);
-    },
-    goLoginPage() {
-      this.$router.push({ path: "/" });
     },
     confirmWallet() {
       console.log("walletを見る");
